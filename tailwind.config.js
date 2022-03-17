c709102aad0ef5d19b170e37648d7399092109bb
MatheusFLemma <matheusflemma123@gmail.com>
2022-03-17 11:09:23 -0300

    feat: adiçao do axios e tema da aplicaçao

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      dark: '#171924',
+      blue: '#3861FB',
+      gray: '#D8D8D8',
+      'light-gray': '#F2F2F2',
+      white: '#FFFFFF',
+    },
+    fontSize: {
+      '6.5xl': '4rem',
+    },
   },
   plugins: [],
 };
