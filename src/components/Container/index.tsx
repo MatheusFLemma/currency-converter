f5cb09f793fd06208d187311b3ed3e9e8b1a5d0e
MatheusFLemma <matheusflemma123@gmail.com>
2022-03-21 10:44:38 -0300

    feat: criaçao do componente container para alocar todos os outros componentes da aplicaçao

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,9 +1,11 @@
 import Heading from '../Heading';
+import CurrencyInput from '../CurrencyInput/index';
 
 function Container() {
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-evenly">
+    <main className="w-screen h-screen flex flex-col items-center justify-evenly container mx-auto max-w-5xl">
       <Heading>Conversor de Moedas</Heading>
+      <CurrencyInput />
     </main>
   );
 }
